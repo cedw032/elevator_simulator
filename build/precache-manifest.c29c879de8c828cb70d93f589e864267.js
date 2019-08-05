d360f91d2dbf0ae80e66bc5901a9529ae562855d
Chad Edwards <chad@fergusapp.com>
2019-08-06 09:29:00 +1200

    Build and deploy

---
+++ build/precache-manifest.c29c879de8c828cb70d93f589e864267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52194291f026e739dc1247fa90816ee7",
+    "url": "/elevator_simulator/build/index.html"
+  },
+  {
+    "revision": "cef85fbcb294ba99ca5e",
+    "url": "/elevator_simulator/build/static/css/main.3e2585b1.chunk.css"
+  },
+  {
+    "revision": "e5dfe50d546985bafa64",
+    "url": "/elevator_simulator/build/static/js/2.97f5ed75.chunk.js"
+  },
+  {
+    "revision": "cef85fbcb294ba99ca5e",
+    "url": "/elevator_simulator/build/static/js/main.a3f561d4.chunk.js"
+  },
+  {
+    "revision": "cd4380db904a2cb30673",
+    "url": "/elevator_simulator/build/static/js/runtime~main.b58e3040.js"
+  }
+]);
